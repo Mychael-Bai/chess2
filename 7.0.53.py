de85cdf266b83c4a784351e4437701b4e71c1ae8
Mychael-Bai <1123534949@qq.com>
2025-03-06 14:18:53 +0800

    Update 7.0.53.py

--- 7.0.53.py
+++ 7.0.53.py
@@ -20,6 +20,7 @@ class ChineseChess:
         self.board_copy = [[None for _ in range(9)] for _ in range(10)]  # Initialize empty board copy
         self.copy_switch_board = [[None for _ in range(9)] for _ in range(10)]  # Initialize empty board copy
         
+        
         self.original_piece_info = None
         self.board_piece_info = None
         
